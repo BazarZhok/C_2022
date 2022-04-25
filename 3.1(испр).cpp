f1032c8a54fb2b084be1368e60f9664ec5e891e1
BazarZhok <104269300+BazarZhok@users.noreply.github.com>
2022-04-25 22:51:09 +0800

    Create 3.1(испр).cpp

---
+++ 3.1(испр).cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+   int a, b, i;
+   FILE *t;
+   t = fopen("output","w");
+   
+   printf("A and B: ");
+   scanf("%d %d", &a, &b);
+   printf("A: %d\nB: %d\n", a, b);
+    {
+        int s;
+        for (int i=a; a<=b; a++)
+        {
+            s=a*3;
+            {
+                fprintf(t, "s: %d\n", s);
+            }
+        }
+    }     
+
+   fclose(t);
+
+   return 0;
+}
