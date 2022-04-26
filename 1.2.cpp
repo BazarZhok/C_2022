5f47d126c2b35a12c16dda2333093ccd44931638
BazarZhok <104269300+BazarZhok@users.noreply.github.com>
2022-04-26 12:23:20 +0800

    Create 1.2.cpp

---
+++ 1.2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    float b;
+    double c;
+    long d;
+    float s, v, e, f;
+    cin >> a >> b >> c>> d;
+    s = a + b;
+    h = c / d;
+    g = b * c;
+    z = a - d;
+    cout << "сумма = " << s << endl;
+    cout << "деление = " << h << endl;
+    cout << "произведение = " << g << endl;
+    cout << "разность = " << z << endl;
+    return 0;
+}
